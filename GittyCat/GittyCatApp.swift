a473cbb9f2424de20d35cdb317ec70702c599983
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2025-10-07 15:58:58 +0530

    Initial Commit

---
+++ GittyCat/GittyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittyCatApp.swift
+//  GittyCat
+//
+//  Created by Krushn Dayshmookh on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GittyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
